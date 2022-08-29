2ce3bc3173ef375027e5d0a9d15e1ffbac33c1d5
ekanshgupta981 <ekanshgurmon@gmail.com>
2022-08-29 14:32:25 +0530

    navbar-2nd update

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,20 +1,21 @@
 import React from "react";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import KeyboardArrowDownOutlinedIcon from "@mui/icons-material/KeyboardArrowDownOutlined";
 
 export default function Navbar() {
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-light nav-blue">
-        <div className="container-fluid">
-          <div className="d-flex flex-column">
-            <a className="navbar-brand pb-0" href="#">
+        <div className="container-fluid d-flex w-75">
+          <div className="d-flex flex-column ">
+            <a className="navbar-brand pb-0 " href="#">
               <img
                 src="https://static-assets-web.flixcart.com/fk-p-linchpin-web/fk-cp-zion/img/flipkart-plus_8d85f4.png"
                 alt=""
-                className="h-25 w-25"
+                className="filpkart-icon"
               />
             </a>
-            <a className="text-light fst-italic">
+            <a className="text-light fst-italic hover-anchor-underline">
               Explore <span className="Explore_Plus"> Plus</span>
               <img
                 src="https://static-assets-web.flixcart.com/fk-p-linchpin-web/fk-cp-zion/img/plus_aef861.png"
@@ -26,7 +27,7 @@ export default function Navbar() {
 
           <form className="d-flex width-Searchbar ">
             <input
-              className="form-control me-2 search-Bar-shadow p-2"
+              className="form-control me-2 search-Bar-shadow p-2 ps-3"
               type="search"
               placeholder="Search for products, brands and more"
               aria-label="Search"
@@ -45,24 +46,32 @@ export default function Navbar() {
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0  d-flex align-items-center">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0  d-flex align-items-center justify-content-evenly w-100">
               <li className="nav-item">
                 <button className="btn btn-light Login-Button">Login</button>
               </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Link
+              <li className="nav-item ">
+                <a className="nav-link text-light fw-bold" href="#">
+                  Become a Seller
                 </a>
               </li>
 
               <li className="nav-item">
                 <a
-                  className="nav-link disabled"
+                  className="nav-link text-light fw-bold"
                   href="#"
-                  tabindex="-1"
-                  aria-disabled="true"
+                  tabIndex="-1"
                 >
-                  Disabled
+                  More <KeyboardArrowDownOutlinedIcon />
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="nav-link text-light fw-bold"
+                  href="#"
+                  tabIndex="-1"
+                >
+                  <i class="fa fa-shopping-cart" aria-hidden="true"></i> Cart
                 </a>
               </li>
             </ul>
