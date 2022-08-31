1f78ba18378a0e5a3941661ab924f07fd6e6efc8
Digvijay Kadam <digvi@Digvijays-MacBook-Air.local>
2022-08-31 18:02:33 +0530

    cart ui

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import "./index.css";
 import Navbar from "./Components/Navbar";
 import Homepage from "./Components/Homepage/Homepage";
 import ProductsDetails from "./Components/Products/ProductsDetails";
+import Cart from "./Components/cart/Cart";
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Routes>
+      {/* <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/productOverview" element={<ProductsDetails />} />
-      </Routes>
+      </Routes> */}
+      <Cart/>
     </div>
   );
 }
