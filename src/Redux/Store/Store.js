ced42e438aa6165d5caf209376895f28c91eedbe
ShehrozEdu <battshehrozedu@gmail.com>
2022-09-02 12:38:01 +0530

    Redux Folder Str 12:37

--- src/Redux/Store/Store.js
+++ src/Redux/Store/Store.js
@@ -1,1 +1,11 @@
-impor;
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+const middleware = [thunk];
+const store = createStore(
+  reducer,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
